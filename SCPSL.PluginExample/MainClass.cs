f600e5f8e1098b528a768d4fdfe2952c2c643cd9
Lumix <contactlumix@alwaysdata.net>
2024-06-20 14:11:56 +0200

    Add files via upload

---
+++ SCPSL.PluginExample/MainClass.cs
@@ -0,0 +1,11 @@
+﻿using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+
+public class MainClass
+{
+	[PluginEntryPoint("Template", "1.0.0", "Template plugin.", "Northwood")]
+	public void Entry()
+	{
+		Log.Info("Execute");
+	}
+}
